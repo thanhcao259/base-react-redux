2a679cc4f19200434e8806df23f7d19b8da30196
Cao Thanh <thanhcao9501@gmail.com>
2024-11-07 17:45:59 +0700

    onSubmit form, simple onChange() of input

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,28 +6,47 @@ class MyComponent extends React.Component {
         name: 'Ronando', address: 'VN', age: 30
     }
 
-    handleClick(event){
+    handleClick(event) {
         console.log(`>>> Clicked button ${this.state.age}`);
         this.setState({
-            age: Math.floor((Math.random() * 100)+1)
+            age: Math.floor((Math.random() * 100) + 1)
         })
     }
-    
+
     handleMouseOver(event) {
         console.log(event.target);
         console.log(`>>> Clicked button ${this.state.age}`);
         // console.log(event);
+    }
+
+    handleOnChangeInput = (event) => {
+        // console.log(event.target.value);
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnSubmit = (e)=>{
+        e.preventDefault();
+        alert('submited')
     }
     render() {
         return (
             <div>
                 My name is {this.state.name}
                 {/* Nen viet onClick.... theo arrow func */}
-                <button className='btn-click' 
-                    onClick={(e)=>{this.handleClick(e)}}>Click</button>
+                <button className='btn-click'
+                    onClick={(e) => this.handleClick(e)}>Click</button>
+                
                 {/* Khong nen viet ntn */}
-                <button className='btn-hover' 
-                    onMouseOver={this.handleMouseOver}>Hover Me</button>
+                {/* <button className='btn-hover'
+                    onMouseOver={this.handleMouseOver}>Hover Me</button> */}
+
+                <form onSubmit={(e)=>{this.handleOnSubmit(e)}}>
+                    <input type='text'
+                        onChange={(e) => {this.handleOnChangeInput(e)} }></input>
+                    <button >Submit</button>
+                </form>
             </div>
         )
     }
