030e8537ad10fd75d50016c3b106314ea6540ce0
Cao Thanh <thanhcao9501@gmail.com>
2024-11-07 17:09:49 +0700

    DOM event click

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,10 +5,24 @@ class MyComponent extends React.Component {
     state = {
         name: 'Ronando', address: 'VN', age: 30
     }
+
+    handleClick(event){
+        console.log(`>>> Clicked button ${this.state.age}`);
+        console.log(event.target);
+    }
+
+    handleMouseOver(event) {
+        console.log(event.target);
+        // console.log(event);
+    }
     render() {
         return (
             <div>
                 My name is {this.state.name}
+                <button className='btn-click' 
+                    onClick={(e)=>{this.handleClick(e)}}>Click</button>
+                <button className='btn-hover' 
+                    onMouseOver={this.handleMouseOver}>Hover Me</button>
             </div>
         )
     }
