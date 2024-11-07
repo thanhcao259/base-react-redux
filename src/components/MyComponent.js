eeb1b15d2538afa99af4cae20111483b58d86666
Cao Thanh <thanhcao9501@gmail.com>
2024-11-07 17:26:06 +0700

    setState for properties on handleClick()

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,19 +8,24 @@ class MyComponent extends React.Component {
 
     handleClick(event){
         console.log(`>>> Clicked button ${this.state.age}`);
-        console.log(event.target);
+        this.setState({
+            age: Math.floor((Math.random() * 100)+1)
+        })
     }
-
+    
     handleMouseOver(event) {
         console.log(event.target);
+        console.log(`>>> Clicked button ${this.state.age}`);
         // console.log(event);
     }
     render() {
         return (
             <div>
                 My name is {this.state.name}
+                {/* Nen viet onClick.... theo arrow func */}
                 <button className='btn-click' 
                     onClick={(e)=>{this.handleClick(e)}}>Click</button>
+                {/* Khong nen viet ntn */}
                 <button className='btn-hover' 
                     onMouseOver={this.handleMouseOver}>Hover Me</button>
             </div>
