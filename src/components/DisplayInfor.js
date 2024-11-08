7a4866b8bfc53011214555b4b638cc0c3bb9350f
Cao Thanh <thanhcao9501@gmail.com>
2024-11-08 10:44:22 +0700

    apply condition in show/hide toggle

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,28 +1,44 @@
 import React from "react";
 
-class DisplayInfor extends React.Component{
+class DisplayInfor extends React.Component {
 
-    render(){
+    state = {
+        isShow: true
+    }
+    handleShowHide = (e) => {
+        this.setState({
+            isShow: !this.state.isShow
+        })
+    };
+    render() {
         // console.log(this.props);
         /**
          * DESTRUCTURE CODE
          */
         // const {age, name} = this.props;
-        const {listUser} = this.props;
-        return(
+        const { listUser } = this.props;
+        return (
             <>
-            <div>
-                {listUser.map((item, index)=>{
-                    return(
-                    <div key={item.id}>
-                        <div>My name is {item.name}</div>
-                        <div>I'm {item.age} years old</div>
-                        <hr/> 
-                    </div>
-         
-                    )
-                })}
-            </div>
+                <div>
+                    <span onClick={() => { this.handleShowHide() }}>
+                        {this.state.isShow ? "Hide " : "Show "}list of users</span>
+                </div>
+                { this.state.isShow && 
+                <div>
+                    {listUser.map((item, index) => {
+
+                        return (
+                        <div key={item.id}
+                            className={+item.age > 28 ? "red" : "green"}>
+                            <div>My name is {item.name}</div>
+                            <div>I'm {item.age} years old</div>
+                            <hr />
+                        </div>
+                        )
+                    })}
+                </div>
+
+                }
             </>
         )
     }
